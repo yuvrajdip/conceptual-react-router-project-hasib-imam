dfe6d265bd903d7da1620de78e32936aed21ff1c
yuvrajdip <dipbhowmik2018@gmail.com>
2023-09-27 11:02:07 +0600

    NavBar NavLink added

--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter} from "react-router-dom"
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home/Home";
+import Favorites from "../components/Favorites/Favorites";
+import Login from "../components/Login/Login";
 
 const Route = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const Route = createBrowserRouter([
       {
         path: "/Home",
         element: <Home></Home>
+      },
+      {
+        path: "/favorites",
+        element: <Favorites></Favorites>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
       }
     ]
   }
--- src/components/Header/NavBar/NavBar.jsx
+++ src/components/Header/NavBar/NavBar.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className='text-right'>
-      <h2 className='text-5xl'>This is NavBar</h2>
+    <div>
+      <nav>
+        <ul className='flex gap-5'>
+          <li>
+            <NavLink
+              to="/Home"
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "text-red-300" : ""
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/favorites"
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "text-red-300" : ""
+              }
+            >
+              Favorites
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/login"
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "text-red-300" : ""
+              }
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
